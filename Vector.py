98944fe03becf9a29bd84696cd55f656eb197e7c
Matt Jones <mattjones315@gmail.com>
2017-08-03 21:08:36 -0700

    vector functionality added

--- Vector.py
+++ Vector.py
@@ -14,8 +14,44 @@ import Protein
 
 class Vector:
 
-    def __init__(l):
+    def __init__(self, l):
         
         # Represent a list as a Vector -- all operations are on a native Python list
         self.vec = l
 
+    def add(self, v2)
+        
+        assert v2.length() == self.length()
+
+        n_l = []
+        for i in self.length(): 
+            n_l.append(self.get(i) + v2.get(i))
+        return Vector(n_l)
+
+    def subtract(self, v2):
+        
+        assert v2.length() == self.length()
+
+        n_l = []
+        for i in self.length():
+            n_l.append(self.get(i) - v2.get(i))
+        return Vector(n_l)
+
+    def dot(self, v2):
+        
+        assert v2.length() == self.length()
+
+        prod = 0
+        for i in self.length():
+            prod += self.get(i) * v2.get(i)
+        return prod
+
+    def cross(self, v2):
+        #TODO: IMPLEMENT CROSS PROD CAPABILITY
+        
+    
+    def get(self, i):
+        return self.vec[i]
+
+    def length(self):
+        return len(self.vec)
